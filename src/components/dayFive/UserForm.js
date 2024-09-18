16b522113e3fe954093d6a42c73fd0798a46269a
Junior Naranjo <juniornaranjo@gmail.com>
2024-09-17 23:12:46 -0500

    feat: se agrega la validación de el input name

--- src/components/dayFive/UserForm.js
+++ src/components/dayFive/UserForm.js
@@ -9,6 +9,10 @@ export default function UserForm() {
   // este setea los valores de los input del form que inician un string en vacio
   const [user, setUser] = useState({ name: "", email: "" });
 
+  // Validación
+  const [errorMessagesName, setErrorMessagesName] = useState("");
+  const [errorMessagesEmail, setErrorMessagesEmail] = useState("");
+
   console.log("editUser", editUser);
   useEffect(() => {
     if (editUser) {
@@ -19,6 +23,11 @@ export default function UserForm() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    if (value.length < 3) {
+      setErrorMessagesName("Nombre no valido");
+    } else {
+      setErrorMessagesName("");
+    }
     setUser({ ...user, [name]: value });
   };
 
@@ -35,25 +44,37 @@ export default function UserForm() {
 
   return (
     <form onSubmit={handleSubmit} className="user-from">
-      <input
-        type="text"
-        name="name"
-        value={user.name}
-        onChange={handleChange}
-        placeholder="Name"
-        className="input-user"
-        required
-      />
-      <input
-        type="email"
-        name="email"
-        value={user.email}
-        onChange={handleChange}
-        placeholder="Email"
-        className="input-user"
-        required
-      />
-      <button type="submit">{editUser ? "Update" : "Add"} User </button>
+      <div>
+        <input
+          type="text"
+          name="name"
+          value={user.name}
+          onChange={handleChange}
+          placeholder="Name"
+          className="input-user"
+          required
+        />
+        <div>
+          <p>{errorMessagesName}</p>
+        </div>
+      </div>
+      <div>
+        <input
+          type="email"
+          name="email"
+          value={user.email}
+          onChange={handleChange}
+          placeholder="Email"
+          className="input-user"
+          required
+        />
+        <div>
+          <p>{errorMessagesEmail}</p>
+        </div>
+      </div>
+      <div>
+        <button type="submit">{editUser ? "Update" : "Add"} User </button>
+      </div>
     </form>
   );
 }
