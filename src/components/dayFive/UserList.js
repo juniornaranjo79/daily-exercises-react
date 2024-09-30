cf63baa4a714380e66fe17de9ba1bfdfe99a453f
Junior Naranjo <juniornaranjo@gmail.com>
2024-09-30 15:16:45 -0500

    feat: se hace una mejora basic para dar info cuando no hay users y accesibilidad para los btns

--- src/components/dayFive/UserList.js
+++ src/components/dayFive/UserList.js
@@ -8,20 +8,29 @@ export default function UserList() {
   return (
     <div className="container">
       <ul className="users-list">
-        {users.map((user) => (
-          <li key={user.id} className="user-card">
-            <div className="user-title-card">
-              <h2>{user.name}</h2>
-              <p>
-                <span className="font-bold">Email:</span> {user.email}
-              </p>
-            </div>
-            <div className="actions-card">
-              <button onClick={() => setUserToEdit(user)}>Edit</button>
-              <button onClick={() => deleteUser(user.id)}>Delete</button>
-            </div>
-          </li>
-        ))}
+        {users.length === 0 ? (
+          <p>No hay usuarios disponibles.</p>
+        ) : (
+          users.map((user) => (
+            <li key={user.id} className="user-card">
+              <div className="user-title-card">
+                <h2>{user.name}</h2>
+                <p>
+                  <span className="font-bold">Email:</span> {user.email}
+                </p>
+              </div>
+              <div className="actions-card">
+                <button
+                  onClick={() => setUserToEdit(user)}
+                  aria-label={`Editar ${user.name}`}
+                >
+                  Edit
+                </button>
+                <button onClick={() => deleteUser(user.id)}>Delete</button>
+              </div>
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
