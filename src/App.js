81401d5e70a7358bbca93185e79213550bb75250
Junior Naranjo <juniornaranjo@gmail.com>
2024-09-30 23:38:33 -0500

    feat: Se realiza el contex de kanban

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import "./App.css";
-import { UserProvider } from "./components/dayFive/Hooks/UserContext/UserContext";
+//import { UserProvider } from "./components/dayFive/Hooks/UserContext/UserContext";
 //import DynamicForm from "./components/dayFour/DynamicForm";
 //import TaskList from "./components/dayThree/TaskList";
 //import Greeting from "./components/dayOne/Greeting";
 //import Count from "./components/dayTwo/Count";
-import UserForm from "./components/dayFive/UserForm";
-import UserList from "./components/dayFive/UserList";
+//import UserForm from "./components/dayFive/UserForm";
+//import UserList from "./components/dayFive/UserList";
+import { KanbanProvider } from "./components/daySix/Hooks/KanbanContext";
 
 function App() {
   //const [name, setName] = useState("Junior");
 
   return (
-    <UserProvider>
+    <KanbanProvider>
       <div className="App">
         {/* <Greeting name={name} /> */}
         {/* <Count number={0} /> */}
         {/* <TaskList /> */}
         {/* <DynamicForm /> */}
-        <h1>User Management</h1>
+        {/* <h1>User Management</h1>
         <UserForm />
-        <UserList />
+        <UserList /> */}
       </div>
-    </UserProvider>
+    </KanbanProvider>
   );
 }
 
